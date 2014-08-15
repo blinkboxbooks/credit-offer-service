bde24c03d34e82964f539dcbecd9fcde1026c221
Jan Stette <jans@blinkbox.com>
2014-08-15 15:32:51 +0100

    Added initial code for sending messages, updated app configs and init code.

--- src/main/scala/com/blinkbox/books/creditoffer/EventSenders.scala
+++ src/main/scala/com/blinkbox/books/creditoffer/EventSenders.scala
@@ -1,6 +1,7 @@
 package com.blinkbox.books.creditoffer
 
 import akka.actor.ActorRef
+import akka.pattern.ask
 import akka.util.Timeout
 import com.blinkbox.books.messaging.Event
 import com.blinkbox.books.messaging.EventHeader
@@ -29,18 +30,73 @@ object EventSender {
 }
 
 // Publish Email XML message to Mailer's exchange. 
-class MailerEventSender(delegate: ActorRef)(implicit ec: ExecutionContext, timeout: Timeout) extends EventSender {
+class MailerEventSender(delegate: ActorRef, templateName: String, routingId: String)(implicit ec: ExecutionContext, timeout: Timeout) extends EventSender {
 
-  override def sendEvent(user: User, creditedAmount: Money, offer: String) = ???
+  override def sendEvent(user: User, creditedAmount: Money, offer: String) = {
+    val content = buildEmailContent(user, creditedAmount, offer)
+    delegate ? Event.xml(content, EventHeader(EventSender.Originator))
+  }
+
+  /** Generate output message in the old Mailer format. */
+  private def buildEmailContent(user: User, creditedAmount: Money, offer: String): String = {
+    val XmlDeclaration = """<?xml version="1.0" encoding="UTF-8"?>""" + "\n"
+    val xml =
+      <sendEmail r:messageId={ s"user-${user.id.value}-credited-$offer" } r:instance={ routingId } r:originator="bookStore" xmlns="http://schemas.blinkbox.com/books/emails/sending/v1" xmlns:r="http://schemas.blinkbox.com/books/routing/v1">
+        <template>{ templateName }</template>
+        <to>
+          <recipient>
+            <name>{ user.firstName }</name>
+            <email>{ user.username }</email>
+          </recipient>
+        </to>
+        <templateVariables>
+          <templateVariable>
+            <key>salutation</key>
+            <value>{ user.firstName }</value>
+          </templateVariable>
+          <templateVariable>
+            <key>amountCredited</key>
+            <value>{ creditedAmount.getAmount }</value>
+          </templateVariable>
+        </templateVariables>
+      </sendEmail>
+
+    XmlDeclaration + xml.toString
+  }
 
 }
 
-// Publish Exact Target JSON message to Agora header exchange. 
-class ExactTargetEventSender(delegate: ActorRef)(implicit ec: ExecutionContext, timeout: Timeout) extends EventSender {
+// Publish new-style JSON message to Agora header exchange.
+class EmailEventSender(delegate: ActorRef, templateName: String)(implicit ec: ExecutionContext, timeout: Timeout) extends EventSender {
 
-  override def sendEvent(user: User, creditedAmount: Money, offer: String) = ???
+  override def sendEvent(user: User, creditedAmount: Money, offer: String) = {
+    val attributes = Map("name" -> user.username, "amount" -> creditedAmount.getAmount.toString)
+    val emailTrigger = Email.Send(DateTime.now(DateTimeZone.UTC), Email.User(user.username, user.id.value.toString /*??*/ ),
+      templateName, attributes)
+    delegate ! Event.json(EventHeader(EventSender.Originator), emailTrigger)
+  }
 
 }
+
+// -------------- TODO - TEMPORARY: replace this with actual message schemas when available.
+object Email {
+  import com.blinkbox.books.messaging._
+
+  case class User(emailAddress: String, id: String)
+  case class Send(timestamp: DateTime, to: User, emailTemplateName: String, attributes: Map[String, String])
+
+  implicit object Send extends JsonEventBody[Send] {
+    val jsonMediaType = MediaType("application/vnd.blinkbox.books.events.email.send.v2+json")
+    def unapply(body: EventBody): Option[(DateTime, User, String, Map[String, String])] =
+      JsonEventBody.unapply[Send](body).flatMap(Send.unapply)
+  }
+
+  implicit object User extends JsonEventBody[User] {
+    val jsonMediaType = MediaType("application/vnd.blinkbox.books.events.email.common.v2+json")
+    def unapply(body: EventBody): Option[Any] = JsonEventBody.unapply[User](body).flatMap(User.unapply)
+  }
+}
+// --------------
 
 // Publish User.Credited JSON message to Agora header exchange.
 class ReportingEventSender(delegate: ActorRef)(implicit ec: ExecutionContext, timeout: Timeout) extends EventSender {
@@ -48,14 +104,13 @@ class ReportingEventSender(delegate: ActorRef)(implicit ec: ExecutionContext, ti
   override def sendEvent(user: User, creditedAmount: Money, offer: String) = {
     val creditEvent = User.Credited(DateTime.now(DateTimeZone.UTC), user,
       creditedAmount.getAmount, creditedAmount.getCurrencyUnit.getCurrencyCode, offer)
-    val header = EventHeader(EventSender.Originator)
-    delegate ! Event.json(header, creditEvent)
+    delegate ! Event.json(EventHeader(EventSender.Originator), creditEvent)
   }
 
 }
 
 /** Pass on event to a number of event senders. */
-class CompoundEventSender(delegates: Seq[EventSender]) extends EventSender {
+class CompoundEventSender(delegates: EventSender*) extends EventSender {
   override def sendEvent(user: User, creditedAmount: Money, offer: String) =
     delegates.foreach(delegate => Try(delegate.sendEvent(user, creditedAmount, offer)))
 }

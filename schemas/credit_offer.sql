7990e6c72f4eb3c9cb9aca3cc99d833dd3aed40b
Mohammed Abdulrazeg <mohammed@blinkbox.com>
2014-08-19 15:14:33 +0100

    Improved the schema

--- schemas/credit_offer.sql
+++ schemas/credit_offer.sql
@@ -1,24 +1,6 @@
-/*
- Source Server         : localhost
- Source Server Type    : MySQL
- Source Server Version : 50614
- Source Host           : localhost
- Source Database       : credit_offer
-
- Target Server Type    : MySQL
- Target Server Version : 50614
- File Encoding         : utf-8
-
- Date: 08/19/2014 14:33:17 PM
-*/
-
-SET NAMES utf8;
-SET FOREIGN_KEY_CHECKS = 0;
-
 -- ----------------------------
 --  Table structure for `promotions`
 -- ----------------------------
-DROP TABLE IF EXISTS `promotions`;
 CREATE TABLE `promotions` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `user_id` int(11) NOT NULL,

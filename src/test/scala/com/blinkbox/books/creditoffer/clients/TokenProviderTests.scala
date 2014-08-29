cc941e9781ebb3052ec7f4c668e9273d0aa0eaf8
Egidijus Jankauskas <egidijus@blinkbox.com>
2014-08-29 16:52:57 +0100

    More TokenProvider tests

--- src/test/scala/com/blinkbox/books/creditoffer/clients/TokenProviderTests.scala
+++ src/test/scala/com/blinkbox/books/creditoffer/clients/TokenProviderTests.scala
@@ -1,19 +1,27 @@
 package com.blinkbox.books.creditoffer.clients
 
+import akka.actor.ActorSystem
+import akka.pattern.AskTimeoutException
+import akka.testkit.{ImplicitSender, TestKit, TestProbe}
+import akka.util.Timeout
 import com.blinkbox.books.clients.UnauthorizedException
 import com.blinkbox.books.test.MockitoSyrup
 import com.typesafe.scalalogging.slf4j.StrictLogging
-import org.scalatest.FunSuite
+import org.scalatest.FunSuiteLike
 import org.mockito.Mockito._
-import org.scalatest.concurrent.ScalaFutures
+import org.scalatest.concurrent.{AsyncAssertions, ScalaFutures}
+import org.scalatest.time.{Millis, Seconds, Span}
 import scala.concurrent.ExecutionContext.Implicits.global
+import scala.concurrent.duration._
 
 import scala.concurrent.Future
+import scala.util.{Failure, Success}
 
-class TokenProviderTests extends FunSuite with ScalaFutures with MockitoSyrup {
+class TokenProviderTests extends TestKit(ActorSystem("test-system")) with ImplicitSender with FunSuiteLike with ScalaFutures with AsyncAssertions with MockitoSyrup {
 
+  implicit val defaultPatience = PatienceConfig(timeout = scaled(Span(5, Seconds)), interval = scaled(Span(25, Millis)))
 
-  class WhenRetryTester(val tokenProvider: TokenProvider) extends StrictLogging with AuthRetry {
+  class AuthRetryTester(val tokenProvider: TokenProvider) extends StrictLogging with AuthRetry {
     def getToken(token: String): Future[String] = Future.successful(token)
 
     def triggerRetry(): Future[String] = withAuthRetry(getToken)
@@ -25,10 +33,53 @@ class TokenProviderTests extends FunSuite with ScalaFutures with MockitoSyrup {
     when(tokenProvider.accessToken).thenReturn(Future.failed(UnauthorizedException("test 401", Map.empty)))
     when(tokenProvider.refreshedAccessToken).thenReturn(Future.successful(AccessToken("refreshedToken")))
 
-    val tester = new WhenRetryTester(tokenProvider)
+    val tester = new AuthRetryTester(tokenProvider)
 
     whenReady(tester.triggerRetry()) { res =>
       assert(res == "refreshedToken")
+    }
+
+    verify(tokenProvider).accessToken
+    verify(tokenProvider).refreshedAccessToken
+  }
+
+  test("AuthRetry does not retry if the first attempt fails with something other than UnauthorizedException") {
+    val tokenProvider = mock[TokenProvider]
+
+    when(tokenProvider.accessToken).thenReturn(Future.failed(new RuntimeException("test exception")))
+
+    val tester = new AuthRetryTester(tokenProvider)
+
+    expectException[RuntimeException](tester.triggerRetry())
+
+    verify(tokenProvider).accessToken
+    verifyNoMoreInteractions(tokenProvider)
+  }
+
+  test("ZuulTokenProvider throws AskTimeoutException if TokenProvider actor does not return in time") {
+    val probe = TestProbe()
+    val zuulTokenProvider = new ZuulTokenProvider(probe.ref, Timeout(1.second))
+    expectException[AskTimeoutException](zuulTokenProvider.accessToken)
+  }
+
+  test("AuthRetry does not retry if the first attempt times out") {
+    val probe = TestProbe()
+    val zuulTokenProvider = new ZuulTokenProvider(probe.ref, Timeout(1.second))
+    val tester = new AuthRetryTester(zuulTokenProvider)
+
+    expectException[AskTimeoutException](tester.triggerRetry())
+  }
+
+  private def expectException[T <: Throwable : Manifest](f: Future[_]): T = {
+    val w = new Waiter
+
+    f onComplete {
+      case Success(_) => w.dismiss()
+      case Failure(e) => w(throw e); w.dismiss()
+    }
+
+    intercept[T] {
+      w.await
     }
   }
 }

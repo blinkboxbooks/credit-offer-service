d449358199a27c8ba1c35e7489f37ad19ce66c7b
Egidijus Jankauskas <egidijus@blinkbox.com>
2014-10-15 11:46:26 +0100

    Improved logging

--- src/main/scala/com/blinkbox/books/creditoffer/OfferHistoryService.scala
+++ src/main/scala/com/blinkbox/books/creditoffer/OfferHistoryService.scala
@@ -28,26 +28,31 @@ class DefaultOfferHistoryService[DbTypes <: DatabaseTypes](
     db.withTransaction { implicit session =>
       val newTotalCreditAmount = promotionRepo.totalCreditedAmount.plus(creditAmount)
       // Check the offer has not been given beforehand and that adding it will not exceed the credit limits
-      val canOffer = !isGranted(userId, offerId) &&
-        (newTotalCreditAmount.isLessThan(creditLimit) || newTotalCreditAmount.isEqual(creditLimit))
-      val grantResult = if (canOffer) {
+      val canOffer = !isGranted(userId, offerId) && ! exceedsCreditLimit(newTotalCreditAmount)
+      if (canOffer) {
         val createdTime = DateTime.now(DateTimeZone.UTC)
         promotionRepo.insert(new Promotion(userId, offerId, createdTime, creditAmount))
         Some(GrantedOffer(userId, offerId, creditAmount, createdTime))
       } else {
         None
       }
-      logger.info(s"Granted offer $offerId for user with id $userId: ${grantResult.nonEmpty}")
-      grantResult
     }
 
-  def isGranted(userId: Int, offerId: String)(implicit session: DbTypes#Session): Boolean =
-    promotionRepo.findByUserIdAndOfferId(userId, offerId).isDefined
+  def isGranted(userId: Int, offerId: String)(implicit session: DbTypes#Session): Boolean = {
+    val granted = promotionRepo.findByUserIdAndOfferId(userId, offerId).isDefined
+    logger.info(s"Already granted offer $offerId for user with id $userId: $granted")
+    granted
+  }
+
+  private def exceedsCreditLimit(amount: Money) = {
+    val limitReached = amount.isGreaterThan(creditLimit)
+    if (limitReached) logger.info("Credit limit reached")
+    limitReached
+  }
 
   def listGrantedOffersForUser(userId: Int): Seq[GrantedOffer] =
     db.withSession { implicit session =>
       promotionRepo.findGrantedOffersForUser(userId).map(promotion =>
         new GrantedOffer(userId, promotion.offerId, promotion.creditedAmount, promotion.createdAt))
     }
-
 }

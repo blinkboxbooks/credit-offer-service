1ce93ff63b5a36e2be63cb414bd7c493337f546c
Egidijus Jankauskas <egidijus@blinkbox.com>
2014-10-15 15:52:40 +0100

    Addressed PR comments

--- src/main/scala/com/blinkbox/books/creditoffer/OfferHistoryService.scala
+++ src/main/scala/com/blinkbox/books/creditoffer/OfferHistoryService.scala
@@ -28,27 +28,25 @@ class DefaultOfferHistoryService[DbTypes <: DatabaseTypes](
     db.withTransaction { implicit session =>
       val newTotalCreditAmount = promotionRepo.totalCreditedAmount.plus(creditAmount)
       // Check the offer has not been given beforehand and that adding it will not exceed the credit limits
-      val canOffer = !isGranted(userId, offerId) && ! exceedsCreditLimit(newTotalCreditAmount)
+      val canOffer = if (isGranted(userId, offerId)) {
+        logger.warn(s"$userId has already been granted offer $offerId")
+        false
+      } else if (newTotalCreditAmount.isGreaterThan(creditLimit)) {
+        logger.warn(s"$userId will not be granted offer $offerId because it has reached the limit")
+        false
+      } else true
       if (canOffer) {
         val createdTime = DateTime.now(DateTimeZone.UTC)
         promotionRepo.insert(new Promotion(userId, offerId, createdTime, creditAmount))
+        logger.info(s"Granted offer $offerId for user $userId")
         Some(GrantedOffer(userId, offerId, creditAmount, createdTime))
       } else {
         None
       }
     }
 
-  def isGranted(userId: Int, offerId: String)(implicit session: DbTypes#Session): Boolean = {
-    val granted = promotionRepo.findByUserIdAndOfferId(userId, offerId).isDefined
-    logger.info(s"Already granted offer $offerId for user with id $userId: $granted")
-    granted
-  }
-
-  private def exceedsCreditLimit(amount: Money) = {
-    val limitReached = amount.isGreaterThan(creditLimit)
-    if (limitReached) logger.info("Credit limit reached")
-    limitReached
-  }
+  def isGranted(userId: Int, offerId: String)(implicit session: DbTypes#Session): Boolean =
+    promotionRepo.findByUserIdAndOfferId(userId, offerId).isDefined
 
   def listGrantedOffersForUser(userId: Int): Seq[GrantedOffer] =
     db.withSession { implicit session =>
